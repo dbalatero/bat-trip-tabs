190580beae40d52635a21541acba858f856c060c
David Balatero <dbalatero@gmail.com>
2015-01-26 09:04:19 -0800

    riffs

--- app.rb
+++ app.rb
@@ -1,3 +1,20 @@
 get '/' do
-  "Sinatra Heroku Cedar Template - The bare minimum for a sinatra app on cedar, running rack, and using bundler."
+  max_fret = 16
+  riff_length = 5 + (14 * rand).to_i
+
+  numbers = riff_length.times.map { (max_fret * rand).to_i + 1 }
+  strings = %w[E A D G B E]
+
+  line = "--" + numbers.map { |num| "#{num}--" }.join
+
+  tab = strings.map { |string| "#{string}|#{line}" }.join("\n")
+
+  <<-EOF
+  <h1>King Tears Bat Trip Riff Generator</h1>
+  <pre>#{tab}</pre>
+  <p>
+    Refresh for more riffs.<br />
+    &mdash; <a href="https://twitter.com/dbalatero">@dbalatero</a>
+  </p>
+  EOF
 end
